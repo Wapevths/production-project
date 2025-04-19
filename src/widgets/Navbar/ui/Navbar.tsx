3c8b7a25e22c85c4cca58ea4a932f2155b96d180
alexei <alex.zabolotnik@yandex.ru>
2025-04-19 21:27:33 +0700

    added navbar and decomposition

---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,31 @@
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from './Navbar.module.scss'
+import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+
+interface NavbarProps {
+    className?: string;
+}
+
+export const Navbar = ({className}:NavbarProps) => {
+    return (
+        <div className={classNames(cls.Navbar, {}, [className])}>
+            <img src="" alt=""/>
+            <div className={cls.links}>
+
+                <AppLink to={'/'}
+                         theme={AppLinkTheme.SECONDARY}
+                >
+                    Главная
+                </AppLink>
+
+                <AppLink to={'/about'}
+                         theme={AppLinkTheme.SECONDARY}
+                >
+                    О сайте
+                </AppLink>
+            </div>
+
+        </div>
+    );
+};
+
