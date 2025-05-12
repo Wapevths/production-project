18f276647c9624cdbf733cae83f2c49215424741
alexei <alex.zabolotnik@yandex.ru>
2025-05-12 21:30:11 +0700

    add error boundary and button error trigger

---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+import { Button, ThemeButton } from 'shared/ui/Button/Button';
+
+// Компонент для тестирования ErrorBoundary
+export const BugButton = () => {
+    const [error, setError] = useState(false);
+
+    const handleThrow = () => {
+        setError(true);
+    };
+
+    useEffect(() => {
+        if (error) throw new Error();
+    }, [error]);
+
+    return (
+        <Button onClick={handleThrow} theme={ThemeButton.ERROR}>
+            throw error
+        </Button>
+    );
+};
