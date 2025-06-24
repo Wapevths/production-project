c93d69f2a2e89a87cc5f6244dd82fc2e2413628a
waperst <alex.zabolotnik@yandex.ru>
2025-06-24 23:58:01 +0700

    fix build plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -7,7 +7,8 @@ import { BuildOptions } from './types/config';
 export function buildPlugins(
     { paths, isDev }: BuildOptions,
 ): webpack.WebpackPluginInstance[] {
-    return [
+
+    const plugins = [
         new HtmlWebpackPlugin({
             template: paths.html,
         }),
@@ -19,9 +20,15 @@ export function buildPlugins(
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
         }),
-        new webpack.HotModuleReplacementPlugin(),
-        new BundleAnalyzerPlugin({
+
+    ]
+
+    if (isDev) {
+        plugins.push(new webpack.HotModuleReplacementPlugin())
+        plugins.push(new BundleAnalyzerPlugin({
             openAnalyzer: false,
-        }),
-    ];
+        }))
+    }
+
+    return plugins;
 }
